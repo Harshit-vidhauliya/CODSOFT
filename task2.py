1aadf69a5cecaf9ff868352e93629e9d70d0346e
Harshit-vidhauliya <harshitvidhauliya@gmail.com>
2026-06-17 22:31:49 +0530

    Add simple calculator functionality in task2.py

---
+++ task2.py
@@ -0,0 +1,34 @@
+print("==SIMPLE CALCULATOR ==")
+
+num1 = float(input("Enter First Number: "))
+num2 = float(input("Enter Second Number: "))
+
+print("\nChoose Operation")
+print("1. Addition (+)")
+print("2. Subtraction (-)")
+print("3. Multiplication (*)")
+print("4. Division (/)")
+
+choice = input("Enter Choice (1/2/3/4): ")
+
+if choice == "1":
+    result = num1 + num2
+    print("Result =", result)
+
+elif choice == "2":
+    result = num1 - num2
+    print("Result =", result)
+
+elif choice == "3":
+    result = num1 * num2
+    print("Result =", result)
+
+elif choice == "4":
+    if num2 != 0:
+        result = num1 / num2
+        print("Result =", result)
+    else:
+        print("Division by zero is not allowed!")
+
+else:
+    print("Invalid Choice!")
